a733a711b7dba6b277e3800109b5a9bc0e9ec76a
spart4nicus <paradelovhristiyan@gmail.com>
2019-08-29 19:48:13 +0200

    build_kernel: improve, and switch to higher GCC
    Signed-off-by: spart4nicus <paradelovhristiyan@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,9 +2,9 @@
 
 
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/Android/Toolchains/aarch64-linux-gnu/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=p
 
 make exynos7885-a40_defconfig
-make -j64
+make -j((`nproc`-1))
 
